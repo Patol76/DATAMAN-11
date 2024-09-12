ba8c0e0e5668e5b32fa46e63fbc50ec0819f9172
Patol76 <110019021+Patol76@users.noreply.github.com>
2024-09-12 16:36:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Import page functions
+from home import home_page
+from technical_analysis import technical_analysis_page
+from future_forecast import future_forecast_page # type: ignore
+
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Select navigation bar:", ['Home', 'Technical Analysis', 'Future Forecast'])
+
+    if page == "Home":
+        home_page()
+    elif page == "Technical Analysis":
+        technical_analysis_page()
+    elif page == "Future Forecast":
+        future_forecast_page()
+
+if __name__ == "__main__":
+    main()
